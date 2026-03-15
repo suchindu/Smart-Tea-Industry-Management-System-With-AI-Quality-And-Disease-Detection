cacc27ee0006d7b65a2b37fb22ccd3ec135f8e28
Suchindu Malith <122116912+Suchindu@users.noreply.github.com>
2026-03-16 00:36:33 +0530

    fix error

--- frontend/src/api/teaFlavorQuality.js
+++ frontend/src/api/teaFlavorQuality.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from './axios';
 
-const API_BASE_URL = '/api/tea-flavor-quality';
+const API_BASE_URL = '/tea-flavor-quality';
 
 // Create quality calculation
 export async function createCalculation(data) {
